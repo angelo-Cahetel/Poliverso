4daa4606b6fd42133cba1016fc27ab3aba3d1d05
angelo-Cahetel <angelo030825@gmail.com>
2023-07-17 18:33:46 -0300

    alteracao

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 
 // INTRO
 var introTL = gsap.timeline();
-introTL.to(".intro", .14, {fontFamily: "Anton"})
-introTL.to(".intro", .14, {fontFamily: "Jost"})
-introTL.to(".intro", .14, {fontFamily: "Alkatra"})
-introTL.to(".intro", .14, {fontFamily: "Nova Oval"})
-introTL.to(".intro", .14, {fontFamily: "Oswald"})
-introTL.to(".intro", .14, {fontFamily: "PT Serif"})
-introTL.to(".intro", .14, {fontFamily: "Lexend"})
-introTL.to(".intro", .14, {fontFamily: "Poppins"})
-introTL.to(".intro", .14, {fontFamily: "Titillium Web"})
+// introTL.to(".intro", .14, {fontFamily: "Anton"})
+// introTL.to(".intro", .14, {fontFamily: "Jost"})
+// introTL.to(".intro", .14, {fontFamily: "Alkatra"})
+// introTL.to(".intro", .14, {fontFamily: "Nova Oval"})
+// introTL.to(".intro", .14, {fontFamily: "Oswald"})
+// introTL.to(".intro", .14, {fontFamily: "PT Serif"})
+// introTL.to(".intro", .14, {fontFamily: "Lexend"})
+// introTL.to(".intro", .14, {fontFamily: "Poppins"})
+// introTL.to(".intro", .14, {fontFamily: "Titillium Web"})
 introTL.to(".intro", 1, { scaleY: 0, ease:"expo.inOut"})
 introTL.to(".intro__green", 1, {scaleY: 2, ease:"expo.inOut"}, "-=1.25")
 
@@ -28,19 +28,19 @@ function separateWordsAndLetters(text) {
   return result;
 }
 
-var clipH1 = document.querySelector(".clip h1")
-let separatedClipH1 = separateWordsAndLetters(clipH1.innerHTML);
-clipH1.innerHTML = separatedClipH1;
-var clipH1Letters = document.querySelectorAll(".clip h1 span")
-gsap.set(clipH1Letters, {y: "120%", scale: -.5})
-gsap.to(clipH1Letters, {duration: 1.5, y: "10%", scale: 1, ease: "expo.inOut", delay: 0.6, stagger: 0.025})
+// var clipH1 = document.querySelector(".clip h1")
+// let separatedClipH1 = separateWordsAndLetters(clipH1.innerHTML);
+// clipH1.innerHTML = separatedClipH1;
+// var clipH1Letters = document.querySelectorAll(".clip h1 span")
+// gsap.set(clipH1Letters, {y: "120%", scale: -.5})
+// gsap.to(clipH1Letters, {duration: 1.5, y: "10%", scale: 1, ease: "expo.inOut", delay: 0.6, stagger: 0.025})
 
-var clipH1Stroke = document.querySelector(".clip .h1__stroke")
-let separatedClipH1Stroke = separateWordsAndLetters(clipH1Stroke.innerHTML);
-clipH1Stroke.innerHTML = separatedClipH1Stroke;
-var clipH1StrokeLetters = document.querySelectorAll(".clip .h1__stroke span")
-gsap.set(clipH1StrokeLetters, {y: "120%", scale: -.5})
-gsap.to(clipH1StrokeLetters, {duration: 1.5, y: "10%", scale: 1, ease: "expo.inOut", delay: 0.6, stagger: 0.025})
+// var clipH1Stroke = document.querySelector(".clip .h1__stroke")
+// let separatedClipH1Stroke = separateWordsAndLetters(clipH1Stroke.innerHTML);
+// clipH1Stroke.innerHTML = separatedClipH1Stroke;
+// var clipH1StrokeLetters = document.querySelectorAll(".clip .h1__stroke span")
+// gsap.set(clipH1StrokeLetters, {y: "120%", scale: -.5})
+// gsap.to(clipH1StrokeLetters, {duration: 1.5, y: "10%", scale: 1, ease: "expo.inOut", delay: 0.6, stagger: 0.025})
 
 //CURSOR
 var cursor = document.querySelector(".cursor")
